4f0490760eefbfe4c1307a7a5152f4977e6ae215
Anjali Singh <singhanjali33375@gmail.com>
2026-02-03 15:41:18 +0530

    Create scrape_events.py

---
+++ frontend/frontend/lambda/lambda/scrape_events.py
@@ -0,0 +1,6 @@
+def scrape_events():
+    # Conceptual scraper
+    return [
+        {"name": "Local Art Show", "location": "City Center"},
+        {"name": "Food Festival", "location": "Downtown"}
+    ]
